a29b4d65f37ed5ab193deffaef438a0b644797a1
Ynnocent <plucenaynno@gmail.com>
2025-04-22 19:53:44 -0600

    2nd Commit
    Added Christmas and other stuff for the assignment

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyFirstConsoleApplication
 {
@@ -12,11 +8,19 @@ namespace MyFirstConsoleApplication
         {
             string fname = "Ynno";
             string lname = "Plucena";
+            string location = "Philippines";
             Console.WriteLine($"My name is {fname} {lname}");
+            Console.WriteLine($"I am from {location}");
 
-            
+
             DateTime currentDate = DateTime.Now;
+
             Console.WriteLine($"Current date: {currentDate.ToString("MM/dd/yyyy")}");
+            
+            DateTime christmas = new DateTime(currentDate.Year, 12, 25);
+            TimeSpan daysUntilChristmas = christmas - currentDate;
+            Console.WriteLine($"Number of days until Christmas: {daysUntilChristmas.Days}");
+
 
             // C# Program in Yellow Book
             Console.WriteLine("\n- WINDOW AREA CALCULATOR -");
@@ -29,8 +33,9 @@ namespace MyFirstConsoleApplication
             
             Console.Write("Enter the height of the window: ");
             heightString = Console.ReadLine();
-            
             height = double.Parse(heightString);
+
+
             woodLength = 2 * (width + height) * 3.25;
             glassArea = 2 * (width * height);
             
@@ -40,6 +45,8 @@ namespace MyFirstConsoleApplication
             Console.WriteLine("\nThe area of the glass is " +
             glassArea + " square metres");
 
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
         }
     }
 }
